7a947365ebe3000c5e0580bb9303b3e27089312b
jiangzhouq <jiangzhouq@gmail.com>
2014-09-11 18:31:10 +0800

    改为试验json的demo

--- src/com/example/jzqtest/MainActivity.java
+++ src/com/example/jzqtest/MainActivity.java
@@ -1,169 +1,204 @@
 package com.example.jzqtest;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Set;
-import java.util.UUID;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothServerSocket;
-import android.bluetooth.BluetoothSocket;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends Activity {
+	private Button btn_get;
+	private Button btn_post;
 
-	BluetoothAdapter mBluetoothAdapter;
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Button btn = (Button) findViewById(R.id.btn);
-		btn.setOnClickListener(this);
-		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		btn_get = (Button) findViewById(R.id.btn_get);
+		btn_post = (Button) findViewById(R.id.btn_post);
+		btn_get.setOnClickListener(listener);
+		btn_post.setOnClickListener(listener);
 	}
 
-	@Override
-	public void onClick(View arg0) {
-		if (mBluetoothAdapter != null) {
-			if (!mBluetoothAdapter.isEnabled()) {
-			    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-			    startActivityForResult(enableBtIntent, 1);
-			}else{
-				Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-				// If there are paired devices
-				Log.d("qiqi", "pairedDevices.size():" + pairedDevices.size());
-				if (pairedDevices.size() > 0) {
-				    // Loop through paired devices
-				    for (BluetoothDevice device : pairedDevices) {
-				        // Add the name and address to an array adapter to show in a ListView
-				    	Log.d("qiqi", device.getName() + "=====" + device.getAddress());
-				    	new AcceptThread().run();
-				    }
-				}
-			}
-		}else{
-			
-		}
-	}
-	
-	private class AcceptThread extends Thread {
-	    private final BluetoothServerSocket mmServerSocket;
-	 
-	    public AcceptThread() {
-	        // Use a temporary object that is later assigned to mmServerSocket,
-	        // because mmServerSocket is final
-	        BluetoothServerSocket tmp = null;
-	        try {
-	            // MY_UUID is the app's UUID string, also used by the client code
-	            tmp = mBluetoothAdapter.listenUsingRfcommWithServiceRecord("main", UUID.fromString("71f78e96-3024-11e4-89c4-a6c5e4d22fb7"));
-	        } catch (IOException e) { }
-	        mmServerSocket = tmp;
-	        Log.d("qiqi", "create acceptthread");
-	    }
-	 
-	    public void run() {
-	        BluetoothSocket socket = null;
-	        // Keep listening until exception occurs or a socket is returned
-	        Log.d("qiqi", "run()");
-	        while (true) {
-	            try {
-	            	Log.d("qiqi", "accetping socket");
-	                socket = mmServerSocket.accept();
-	                Log.d("qiqi", "accetping socket 2");
-	            } catch (IOException e) {
-	            	Log.d("qiqi", "" + e.toString());
-	                break;
-	            }
-	            // If a connection was accepted
-	            if (socket != null) {
-	                // Do work to manage the connection (in a separate thread)
-//	                manageConnectedSocket(socket);
-	            	new ConnectedThread(socket).run();
-	                try {
-						mmServerSocket.close();
+	private OnClickListener listener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			switch (v.getId()) {
+			case R.id.btn_post:
+				Runnable postRun = new Runnable() {
+					@Override
+					public void run() {
+						Log.i("TestHttpActivity", "ok");
+						DefaultHttpClient client = new DefaultHttpClient();
+						/** NameValuePair是传送给服务器的请求参数 param.get("name") **/
+						List<NameValuePair> list = new ArrayList<NameValuePair>();
+						NameValuePair pair1 = new BasicNameValuePair("name", "name0001");
+						NameValuePair pair2 = new BasicNameValuePair("age", "age0001");
+						list.add(pair1);
+						list.add(pair2);
+						UrlEncodedFormEntity entity = null;
+						try {
+							/** 设置编码 **/
+							entity = new UrlEncodedFormEntity(list, "UTF-8");
+						} catch (UnsupportedEncodingException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						/** 新建一个post请求 **/
+						HttpPost post = new HttpPost("http://106.186.22.172:12341");
+						post.setEntity(entity);
+						HttpResponse response = null;
+						String strResult = "";
+						try {
+							/** 客服端向服务器发送请求 **/
+							response = client.execute(post);
+						} catch (ClientProtocolException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						/** 请求发送成功，并得到响应 **/
+						if (response.getStatusLine().getStatusCode() == 200) {
+							try {
+								/** 读取服务器返回过来的json字符串数据 **/
+								strResult = EntityUtils.toString(response.getEntity());
+							} catch (IllegalStateException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							Log.i("TestHttpActivity",  strResult);
+
+						} else
+							Log.i("TestHttpActivity",  "POST提交失败");
+					}
+				};
+				new Thread(postRun).start();
+				break;
+
+			case R.id.btn_get:
+				Runnable getRun = new Runnable() {
+					
+					@Override
+					public void run() {
+						DefaultHttpClient client1 = new DefaultHttpClient();
+						/** NameValuePair是传送给服务器的请求参数 param.get("name") **/
+
+						UrlEncodedFormEntity entity1 = null;
+
+						/** 新建一个get请求 **/
+						HttpGet get = new HttpGet("http://106.186.22.172:12341?name=qiqi");
+						HttpResponse response1 = null;
+						String strResult1 = "";
+						try {
+							/** 客服端向服务器发送请求 **/
+							response1 = client1.execute(get);
+						} catch (ClientProtocolException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+				/** 请求发送成功，并得到响应 **/
+				if (response1.getStatusLine().getStatusCode() == 200) {
+					try {
+						/** 读取服务器返回过来的json字符串数据 **/
+						strResult1 = EntityUtils
+								.toString(response1.getEntity());
+					} catch (IllegalStateException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-	                break;
-	            }else{
-	            	Log.d("qiqi", "nul ==  socket");
-	            }
-	        }
-	    }
-	 
-	    /** Will cancel the listening socket, and cause the thread to finish */
-	    public void cancel() {
-	        try {
-	            mmServerSocket.close();
-	        } catch (IOException e) { }
-	    }
+
+					JSONObject jsonObject1 = null;
+					try {
+						/** 把json字符串转换成json对象 **/
+						jsonObject1 = getJSON(strResult1);
+					} catch (JSONException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					String names = "";
+					Log.d("qiqi", strResult1);
+					try {
+						/**
+						 * jsonObject.getString("code") 取出code 比如这里返回的json 字符串为
+						 * [code:0,msg:"ok",data:[list:{"name":1},{"name":2}]]
+						 * **/
+
+						/** 得到data这个key **/
+						String data = jsonObject1.getString("students");
+						/** 把data下的数据转换成json对象 **/
+						JSONArray jarr1 = new JSONArray(data);
+						/** 判断data对象下的list是否存在 **/
+//						if (jDat1.get("list") != null) {
+//							/** 把list转换成jsonArray对象 **/
+//							JSONArray jarr1 = jDat1.getJSONArray("list");
+							/** 循环list对象 **/
+							for (int i = 0; i < jarr1.length(); i++) {
+
+								/** **/
+								JSONObject jsono = (JSONObject) jarr1.get(i);
+
+								/** 取出list下的name的值 **/
+								names = jsono.getString("name");
+								Log.d("qiqi", names);
+//							}
+						}
+//						Toast.makeText(
+//								MainActivity.this,
+//								"get请求： code:" + jsonObject1.getString("code")
+//										+ "name:" + names, Toast.LENGTH_SHORT)
+//								.show();
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+				} else
+//					Toast.makeText(MainActivity.this, "get提交失败",
+//							Toast.LENGTH_SHORT).show();
+					Log.d("qiqi", "get提交失败");
+					}
+				};
+				new Thread(getRun).start();
+				break;
+			}
+
+		}
+	};
+
+	public JSONObject getJSON(String sb) throws JSONException {
+		return new JSONObject(sb);
 	}
-	private class ConnectedThread extends Thread {
-	    private final BluetoothSocket mmSocket;
-	    private final InputStream mmInStream;
-	    private final OutputStream mmOutStream;
-	 
-	    public ConnectedThread(BluetoothSocket socket) {
-	        mmSocket = socket;
-	        InputStream tmpIn = null;
-	        OutputStream tmpOut = null;
-	 
-	        // Get the input and output streams, using temp objects because
-	        // member streams are final
-	        try {
-	            tmpIn = socket.getInputStream();
-	            tmpOut = socket.getOutputStream();
-	        } catch (IOException e) { }
-	 
-	        mmInStream = tmpIn;
-	        mmOutStream = tmpOut;
-	    }
-	 
-	    public void run() {
-	        byte[] buffer = new byte[1024];  // buffer store for the stream
-	        int bytes; // bytes returned from read()
-	 
-	        // Keep listening to the InputStream until an exception occurs
-	        while (true) {
-	            try {
-	                // Read from the InputStream
-	                bytes = mmInStream.read(buffer);
-	                byte[] newBuffer = new byte[bytes];
-	                for(int i = 0; i < bytes; i++){
-	                	newBuffer[i] = buffer[i];
-	                }
-	                
-	                Log.d("qiqi", "hello received:" + new String(newBuffer, "UTF-8") + " bytes:" + bytes);
-	                // Send the obtained bytes to the UI activity
-//	                mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
-//	                        .sendToTarget();
-	            } catch (IOException e) {
-	                break;
-	            }
-	        }
-	    }
-	 
-	    /* Call this from the main activity to send data to the remote device */
-	    public void write(byte[] bytes) {
-	        try {
-	            mmOutStream.write(bytes);
-	        } catch (IOException e) { }
-	    }
-	    /* Call this from the main activity to shutdown the connection */
-	    public void cancel() {
-	        try {
-	            mmSocket.close();
-	        } catch (IOException e) { }
-	    }
-	}
+
 }
